ae5d597670c79fd06a80ad201b90bd0e5250277c
Guillaume Viguier <guillaume@wedus.org>
2009-10-11 13:43:16 -0400

    Added copyright information

--- src/pam_ecafe.c
+++ src/pam_ecafe.c
@@ -1,21 +1,23 @@
-/***
-  This file is part of pam_ecafe.
-
-  pam_ecafe is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  pam_ecafe is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with pam_dbus; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-  USA
- ***/
+/**
+ * Copyright (C) 2009 Guillaume Viguier-Just
+ * 
+ * Author: Guillaume Viguier-Just <guillaume@wedus.org>
+ * 
+ * This file is part of pam_ecafe.
+ * 
+ * pam_ecafe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * pam_ecafe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with pam_ecafe.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #define PAM_SM_AUTH
 
